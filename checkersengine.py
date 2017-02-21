00c4351329cada47a877fef807017fa24edc25f8
dmille56 <donovanm56@gmail.com>
2017-02-21 00:09:09 -0700

    preparing to implement the minimax algorithm

--- checkersengine.py
+++ checkersengine.py
@@ -80,7 +80,6 @@ def evaluateBoard(board, weights):
     return evaluateFeatures(getFeatures(board), weights)
 
 def evaluateBoardRecursive(board, side, turn, blackWeights, whiteWeights, depth=3):
-    #TODO- finish implementing this
     if(depth <= 0):
         if(turn == 'b'):
             return evaluateBoard(board, blackWeights)
@@ -103,6 +102,31 @@ def evaluateBoardRecursive(board, side, turn, blackWeights, whiteWeights, depth=
         a = getBestPossibleBoard(boards, whiteWeights)
         return evaluateBoardRecursive(a, side, 'b', blackWeights,
                 whiteWeights, newDepth) 
+
+def evaluateBoardMinimax(board, side, turn, blackWeights, whiteWeights, depth=3):
+    #TODO- implement the minimax algorithm here
+
+    #pseudocode:
+    #
+    #function minimax(node, depth, maximizingPlayer)
+    #    if depth = 0 or node is a terminal node
+    #        return the heuristic value of node
+
+    #    if maximizingPlayer
+    #        bestValue := −∞
+    #        for each child of node
+    #            v := minimax(child, depth − 1, FALSE)
+    #            bestValue := max(bestValue, v)
+    #        return bestValue
+
+    #    else    (* minimizing player *)
+    #        bestValue := +∞
+    #        for each child of node
+    #            v := minimax(child, depth − 1, TRUE)
+    #            bestValue := min(bestValue, v)
+    #        return bestValue
+
+    pass
 
 def updateWeights(trainingData, weights, didWin):
     n = len(trainingData)
